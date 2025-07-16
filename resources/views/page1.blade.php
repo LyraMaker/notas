07c4dcc8e2f0d9f21c65913492ebc381cf15fa24
LyraMaker <gabriel.lyrat@gmail.com>
2025-07-15 21:56:14 -0300

    Primeiro commit

---
+++ resources/views/page1.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.main_view')
+@section('content')
+<h1>O Ronaldinho! {{ $value }}</h1>
+@endsection
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\MainController;
+
+require __DIR__."/vendor/autoload.php";
+
+echo Controller::class;
